152c140c2d7e809f720924078cf05e6071807a02
JosiahSnarr <josiahs@xlegoz.com>
2015-04-08 16:16:22 -0400

    removed stray .txt

--- Sources/accelerometer.h
+++ Sources/accelerometer.h
@@ -8,3 +8,9 @@ Date: April 8, 2015
 servo.h contains macros, constants, and prototypes for the accelerometer
   functions within accelerometer.c
 *************************************************************************/
+#include "macros.h"
+
+//Definitions
+#define ATD_PORT		  PORTAD
+#define ATD_PORT_DDR	DDRAD
+#define ATD_DDR_INIT  0b00000001
